1f1e31b30a7b618cd5f47d25913648472958ba13
scott-ellenoff <scott-ellenoff@users.noreply.github.com>
2020-10-22 17:50:22 +0100

    Added routes
    I added in the routes and the templates for each of the functions that the routes should call

--- hw-websockets/app.py
+++ hw-websockets/app.py
@@ -1,16 +1,51 @@
 import requests
 import mysql.connector
+from flask import Flask, Response, jsonify
+from flask_cors import CORS
 
 
+app = Flask(__name__)
+CORS(app)
+
+#Various routes
+@app.route('/')
+def index():
+    return testService()
+
+@app.route('/history')
+def history():
+    return historyDeals()
+
+@app.route('/average')
+def average():
+    return groupby_instrument(connect())
+
+@app.route('/dealer/endpos')
+def dealerEndPositions():
+    return endPositions()
+
+@app.route('/dealer/realizer_pl')
+def dealerRealizedPL():
+    return realizedPL()
+
+@app.route('/dealer/effective_pl')
+def dealerEffectivePL():
+    return effectivePL()
+
+@app.route('/client/testservice')
+def testservice():
+    return jsonify(success = True)
+
+#connecting to the database
 def connect():
-    url = "http://127.0.0.1:5000/streamTest/sse"
-    r = requests.get(url, stream=True)
     mydb = mysql.connector.connect(
         host="localhost", user="root", password="ppp", database="db_grad_cs_1917"
     )
     return mydb
 
 
+
+#functions that routes point to
 def groupby_instrument(connection):
     mycursor = connection.cursor()
     sql = "select instrument.instrument_name, averages.deal_type ,averages.val from db_grad_cs_1917.instrument join " \
@@ -22,6 +57,11 @@ def groupby_instrument(connection):
     for x in result:
         print(x)
 
+def historyDeals():
 
-if __name__ == "__main__":
-    groupby_instrument(connect())
+def endPositions():
+
+def realizedPL():
+
+def effectivePL():
+
